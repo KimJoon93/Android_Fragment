c2b475dce88caef7ff2fbd98165bd030d5252442
Kim Joon <jk1094@naver.com>
2019-07-27 18:31:03 +0900

    Add button to open and close Fragment

--- Fragment/app/src/main/java/com/example/fragment/MainActivity.java
+++ Fragment/app/src/main/java/com/example/fragment/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.fragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private boolean isFragmentDisplayed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.open_button);
+
     }
 }
