0e3f6d5c2df7bfbfc281a20eaf56aad5fcf268ae
Kim Joon <jk1094@naver.com>
2019-07-29 18:38:58 +0900

    Define listener interface with callback

--- Fragment_Communication/app/src/main/java/com/example/fragment_communication/SimpleFragment.java
+++ Fragment_Communication/app/src/main/java/com/example/fragment_communication/SimpleFragment.java
@@ -1,6 +1,7 @@
 package com.example.fragment_communication;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +18,12 @@ public class SimpleFragment extends Fragment {
 
     private static final int YES = 0;
     private static final int NO = 1;
+
+    private static final int NONE = 2;
+
+    public int mRadioButtonChoice = NONE;
+
+    OnFragmentInteractionListener mListener;
 
     public SimpleFragment() {
         // Required empty public constructor
@@ -41,13 +48,18 @@ public class SimpleFragment extends Fragment {
                 switch (index) {
                     case YES: // User chose "Yes."
                         textView.setText(R.string.yes_message);
+                        mRadioButtonChoice = YES;
+                        mListener.onRadioButtonChoice(YES);
                         break;
-                        case NO: // User chose "No."
-                            textView.setText(R.string.no_message);
-                            break;
-                            default: // No choice made.
-                                // Do nothing.
-                                break;
+                    case NO: // User chose "No."
+                        textView.setText(R.string.no_message);
+                        mRadioButtonChoice = NO;
+                        mListener.onRadioButtonChoice(NO);
+                        break;
+                    default: // No choice made.
+                        mRadioButtonChoice = NONE;
+                        mListener.onRadioButtonChoice(NONE);
+                        break;
                 }
             }
         });
@@ -65,8 +77,24 @@ public class SimpleFragment extends Fragment {
         return rootView;
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new ClassCastException(context.toString()
+                    + getResources().getString(R.string.exception_message));
+        }
+    }
+
     public static SimpleFragment newInstance() {
         return new SimpleFragment();
+    }
+
+    interface OnFragmentInteractionListener {
+        void onRadioButtonChoice(int choice);
     }
 
 }
